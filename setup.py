a9619f6784c10d2cc09b5ad02b95ca1cd2898e3c
Sam Clements <sam@borntyping.co.uk>
2015-03-07 21:20:45 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tg',
-    version='0.1.0',
+    version='0.2.0',
 
     description='A command line tool for managing your repositories',
     long_description=open('README.md').read(),
